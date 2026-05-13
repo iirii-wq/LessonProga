e06616a99a487e0881709991616963c1ccefdf52
iirii-wq <agoo200844@gmail.com>
2026-05-13 19:48:31 +0300

    Add files via upload

---
+++ sam2.04/1.py
@@ -0,0 +1,8 @@
+def conver_to_miles(km):
+    mili = km * 0.6214
+    return mili
+print(conver_to_miles(1))
+a = (conver_to_miles(5))
+b = (conver_to_miles(10))
+print(f'{a:.3f}')
+print(f'{b:.3f}')
---
+++ sam2.04/3.py
@@ -0,0 +1,4 @@
+def get_middle_pint(x1,y1,x2,y2):
+    return (x1+x2)/2 , (y1+y2)/2
+print(get_middle_pint(0,0,10,0))
+print(get_middle_pint(1,5,8,3))
